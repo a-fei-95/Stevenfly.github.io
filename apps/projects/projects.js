b2856816232d9ca8d3dd122b3e63602e7e2c044c
Steven Yang <stevenwaterloo2015@gmail.com>
2017-10-04 03:25:46 -0400

    update projects page; update experiences page template; update general UI

--- apps/projects/projects.js
+++ apps/projects/projects.js
@@ -4,6 +4,7 @@
     app.controller('ProjectsController', function () {
         this.androidApps = apps;
         this.websites = webs;
+        this.schools = schs;
     });
 
     var apps = [{
@@ -15,6 +16,13 @@
         extra: true,
         extra_image: "https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png",
         extra_link: "https://play.google.com/store/apps/details?id=flystudio.tictactoe&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+    }, {
+        title: "UW HUB",
+        time: "2017",
+        description: "Using University of Waterloo’s API to display and organize useful information to students. The Goal is to gather information in a hub instead of looking for links online individually.",
+        image: "../../assets/placeholder.jpg",
+        alter: "uw_hub",
+        extra: false
     }]
 
 
@@ -34,7 +42,23 @@
         image: "projects/tuttee_preview.png",
         alter: "tuttee",
         link: "http://tuttee.ca",
-        source: false,
-        source_link: ""
+        source: false
+    }, {
+        title: "UW Grade",
+        time: "2017",
+        description: "I have been summarizing my own grades in a self-designed chart to keep track of my academic performances & course planning. I realize I could share this scheme (tailored to my school) to other students.",
+        image: "../../assets/placeholder.jpg",
+        alter: "uw_grade",
+        source: false
+    }]
+
+
+    var schs = [{
+        title: "Watopoly",
+        time: "2016",
+        description: "This was a school project with team of three. The object was to achieve a full monopoly experiences writing in C++++ and displaying board on Terminal.. Each move is written as a command with options.",
+        image: "../../assets/placeholder.jpg",
+        alter: "watopoly",
+        source: false
     }]
 })();
